5b068c4a3f3b8ce4f3896e3a6f75e5bf6ca1a98b
TEST DUPA 1 <jankowski.r@hotmail.com>
2017-03-10 06:57:04 +0100

    Analog almost working

--- sketch_mar10k_led88/sketch_mar10k_led88.ino
+++ sketch_mar10k_led88/sketch_mar10k_led88.ino
@@ -6,8 +6,7 @@ int analogSecondX = -1;
 int analogSecondY = -1;
 int posX = 4;
 int posY = 4;
-int mappedFirstX = -1;
-int mappedFirstY = -1;
+int mappedDestinationX = 0;
 void setup()
 {
   Serial.begin(9600);
@@ -16,29 +15,33 @@ void setup()
 }
 void loop() {
 
-  analogSecondY = analogRead(A10);
-  analogSecondX = analogRead(A11);
+  analogSecondY = analogRead(A11);
+  analogSecondX = analogRead(A10);
 
-  mappedFirstX = map(analogFirstX, 0, 550, -1,1);
-  mappedFirstY = map(analogFirstY, 0, 550, -1,1);
-  posX += mappedFirstX;
-  posY += mappedFirstY;
+  if(analogSecondX > 550){
+    mappedDestinationX = 1;
+    if(analogSecondX > 850)
+    mappedDestinationX = 3;
+    }
+  else if(analogSecondX < 480){
+    mappedDestinationX = -1;
+    if(analogSecondX < 200)
+    mappedDestinationX = -3;
+    }
+  else 
+    mappedDestinationX = 0;
+  posX += mappedDestinationX;
+
   Serial.print(posX);
   Serial.print("/");
   Serial.print(posY);
   Serial.print(" ");
-    Serial.print(mappedFirstX);
-  Serial.print("/");
-  Serial.print(mappedFirstY);
-    Serial.print(" ");
     Serial.print(analogSecondY);
   Serial.print("/");
   Serial.print(analogSecondX);
   Serial.println(" ");
   if(posX < 8 && posY < 8 && posY >= 0 && posX >= 0){
+    led.clearDisplay();
     led.setLed(posX, posY, true);
-  }else{
-    posX = 4;
-    posY = 4;}
-    delay(150);
   }
+}
