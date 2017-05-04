39132a00da49660b0b0f5515fa76a37a8957b44f
Stan Reduta <stanislaw.reduta@gmail.com>
2017-05-04 17:39:18 +0200

    Changes in sonar

--- sonar/sonar.ino
+++ sonar/sonar.ino
@@ -21,19 +21,41 @@ void setup() {
   }
 }
 
-void lightUp(int v){
+void displayDistanceSerial(float distance){
+  Serial.print("Distance (in cm): ");
+  Serial.print(distance);
+  Serial.print(int(distance));
+  Serial.println();
+}
 
+void lightUp(int v){
   for (int i = 0; i < 8; i++){
       digitalWrite(2 + i, 0);
     }
-
   if (v > 9) {
     v = 9; 
   }
-
   for (int i = 0; i < v; i++){
       digitalWrite(2 + i, 1);
     }
+}
+
+void checkBrakeStatus(float distance){
+
+  if(int(distance) < 25){
+    displayDistanceSerial(float distance);
+
+    } else {
+      displayDistanceSerial(float distance);
+//      implement breaking
+//digitalWrite(AIN1, HIGH); // AIN1; 
+//  digitalWrite(AIN2, HIGH); // AIN1; 
+//  digitalWrite(BIN1, HIGH); // AIN2
+//  digitalWrite(BIN2, HIGH); // AIN1;
+
+    }
+}
+
 }
 
 void loop() {
@@ -43,25 +65,22 @@ void loop() {
   digitalWrite(trig, HIGH);
   delayMicroseconds(10);
   digitalWrite(trig, LOW); 
-
+  
   pinMode(echo, INPUT);
   duration = pulseIn(echo, HIGH);
  
   distance = (duration/2) / 29.1 - 0.5;
 
-  Serial.print("Distance (in cm): ");
-  Serial.print(distance);
-  Serial.print(int(distance));
-  Serial.println();
-
+  //LCD monitor  
+  displayDistanceLCD(distance);
   
-  lcd.setCursor(0,0); 
-  lcd.print("Distance (in cm): ");
-  lcd.setCursor(0,1);
-  sprintf(buffor, "%.2f", distance);
-  lcd.print(buffor);
+  //Serial monitor
+  displayDistanceSerial(distance);
+  
 
   lightUp(int(distance-3));
+
+  
   
   
   delay(250);
