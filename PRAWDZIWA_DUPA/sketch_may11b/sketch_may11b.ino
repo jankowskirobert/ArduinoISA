1e0be8ccfde4ec00ca1bbc2b5caeb3f9ee378d95
Robert Jankowski <jankowski.r@hotmail.com>
2017-06-13 10:08:00 +0200

    space ver1

--- PRAWDZIWA_DUPA/sketch_may11b/sketch_may11b.ino
+++ PRAWDZIWA_DUPA/sketch_may11b/sketch_may11b.ino
@@ -12,7 +12,7 @@ ISAButtons button;
 int joy1_x = 0;
 int joy1_y = 0;
 int currentShotPosition[2]  = {0,0};
-
+int currentEnemyShotPosition[2]  = {0,0};
 volatile int** current_position;
 bool leftButtonState = false;
 bool rightButtonState = false;
@@ -21,18 +21,29 @@ unsigned long start_time_RIGHT;
 volatile long long timeout = 200; // 3 seconds
 volatile long long last_change_time = 0;
 bool shotEnable = false;
+bool enemyShotEnable = false;
 bool enemyMoveFlag = false;
 volatile int** enemies;
 volatile int MAX_ENEMY_MOVE = 4;
 volatile int MAX_ENEMY = 8;
 volatile bool endGame = false;
- int hitted = 0;
+int hitted = 0;
+volatile long randNumber;
 
 void setup() {
+  long seed = 0;
+ for (int i = 0; i < 12; i++)
+ seed += analogRead(i);
+ randomSeed(seed);
+ 
   Timer4.attachInterrupt(shotPosition);
   Timer4.start(25000);
   Timer5.attachInterrupt(enemyPosition);
   Timer5.start(1250000);
+  Timer6.attachInterrupt(enemyShotEnabler);
+  Timer6.start(1200000);
+  Timer7.attachInterrupt(enemyShot);
+  Timer7.start(22500);
   oled.begin();
   for (int i = 0 ; i < 7; i++){
     pinMode(LEDS[i], OUTPUT);
@@ -72,7 +83,7 @@ void setup() {
    
   button.init();
   oled.gotoXY(2,2);
-  oled.print("hello"); 
+  oled.print("hello and die"); 
   Serial.begin(9600);
   Serial.println("Hello World");
   oled.renderAll();
@@ -86,9 +97,23 @@ void setup() {
    delay(1000);
   enemyStrategy();
 }
-
+volatile int player_x = 63;
+int joyX = 0;
 void loop() {
-
+   joyX = map(analogRead(A1), 1, 1023, 2, -3);
+   
+   if((player_x<=127) && (player_x>=1)) {    
+        player_x = player_x + joyX/2;
+      }
+      if(player_x > 127) {
+        player_x = 127;
+      }
+      else if(player_x < 1) {
+        player_x = 1;
+      }
+  
+renderPlayerShip();
+      
     
   if(!endGame){
   for(int i = 0 ; i < MAX_ENEMY; i++){
@@ -115,33 +140,42 @@ if(shotEnable){
            oled.setPixel(currentShotPosition[0], currentShotPosition[1]-1, false);
            oled.renderAll();
 }
+if(enemyShotEnable){
+    oled.setPixel(currentEnemyShotPosition[0], currentEnemyShotPosition[1], true);
+        oled.renderAll();
+           oled.setPixel(currentEnemyShotPosition[0], currentEnemyShotPosition[1]-1, false);
+           oled.renderAll();
+}
  oled.clear(false);
     } else {
       oled.clear(true);
     }
-
-
 }
 
+
+
+
+void renderPlayerShip() {
+  for(int i = -2; i < 5; ++i){
+    current_position[i+2][0] = player_x + i;
+  }
+}
 
 void moveShipLeft(){
   int difference = millis()-last_change_time;
-       if(current_position[2][0] > 2 && (((difference)>timeout) || last_change_time == 0)){
-        for(int i = 0; i < 7; ++i){
-        current_position[i][0] -= 3;
-
-        }
+       if(player_x > 3 && (((difference)>timeout) || last_change_time == 0)){  
+          player_x -= 3;
         last_change_time = millis();
      }
+     renderPlayerShip();
 }
 void moveShipRight(){
   int difference = millis()-last_change_time;
-     if(current_position[0][0] < 120 && (((difference)>timeout) || last_change_time == 0)){
-      for(int i = 0; i < 7; ++i){
-      current_position[i][0] += 3;
-      }
+     if(player_x < 122 && (((difference)>timeout) || last_change_time == 0)){
+      player_x += 3;
       last_change_time = millis();
      }
+     renderPlayerShip();
 }
 
 void shot() {
@@ -174,13 +208,33 @@ void checkShot(int posx, int posy){
         endGame = true;
          int hitted = 0;
       }
-//      return true;
     }
   }
   
 }
-
+void enemyShot() {
+  if(enemyShotEnable) {
+      (*(currentEnemyShotPosition+1))++;
+      if((*(currentEnemyShotPosition+1)) > 64){
+        enemyShotEnable = false;
+      }
+      if((*(currentEnemyShotPosition+1)) >= 64 && ((*(currentEnemyShotPosition)) <= player_x+4 && (*(currentEnemyShotPosition)) >= player_x-3)
+      ){
+        endGame = true;
+      }
+  }
+}
+void enemyShotEnabler() {
+  randNumber = random(MAX_ENEMY-1);
+  if(enemies[randNumber][2] == true){
+    enemyShotEnable = true;
+    (*(currentEnemyShotPosition+1)) = enemies[randNumber][1]+8;
+    (*(currentEnemyShotPosition)) = enemies[randNumber][0]+4;
+  }
+}
 void enemyPosition() {
+  
+  
   enemyMoveFlag = !enemyMoveFlag;
   for(int i = 0 ; i < MAX_ENEMY; i++){
     if(enemyMoveFlag)
